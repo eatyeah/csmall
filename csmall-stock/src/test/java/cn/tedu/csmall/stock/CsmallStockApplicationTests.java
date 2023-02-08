d402087ec0a1254f1eb62b93c30aca44a58422d8
sweeterjava <sweeterjava@163.com>
2023-02-08 18:03:22 +0800

    Initial commit

---
+++ csmall-stock/src/test/java/cn/tedu/csmall/stock/CsmallStockApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmallStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
