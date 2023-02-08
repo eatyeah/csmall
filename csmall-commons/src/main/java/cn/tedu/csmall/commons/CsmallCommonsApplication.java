d72a52755580ccba7bc3bdf1f127ab78703c05a9
sweeterjava <sweeterjava@163.com>
2023-02-08 18:20:06 +0800

    添加子项目csmall-commons

---
+++ csmall-commons/src/main/java/cn/tedu/csmall/commons/CsmallCommonsApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmallCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsmallCommonsApplication.class, args);
+    }
+
+}
