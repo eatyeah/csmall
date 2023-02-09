152e7fefba3e599a39b9770614abc1fbcd71a315
sweeterjava <sweeterjava@163.com>
2023-02-09 13:55:31 +0800

    1

---
+++ csmall-cart/src/test/java/cn/tedu/csmall/cart/CsmallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmallCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
