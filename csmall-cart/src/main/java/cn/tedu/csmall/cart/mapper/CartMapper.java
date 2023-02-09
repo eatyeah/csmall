9a129f4bf247a034cd87af32d457fe6a113daa00
sweeterjava <sweeterjava@163.com>
2023-02-09 15:03:26 +0800

    1

---
+++ csmall-cart/src/main/java/cn/tedu/csmall/cart/mapper/CartMapper.java
@@ -0,0 +1,18 @@
+package cn.tedu.csmall.cart.mapper;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Description:
+ * @Author: sweeterjava@163.com
+ * @Date: 2023/2/9
+ * @Time: 14:09
+ */
+
+@Repository
+public interface CartMapper {
+    // 新增购物车商品的方法
+
+    // 删除购物车商品的方法
+
+}
