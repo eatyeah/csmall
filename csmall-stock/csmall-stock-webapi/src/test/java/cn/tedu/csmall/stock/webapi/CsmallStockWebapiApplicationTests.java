5b8855a421d816d353d97cdbec08a82d249e81e1
sweeterjava <sweeterjava@163.com>
2023-02-10 15:59:38 +0800

    1

---
+++ csmall-stock/csmall-stock-webapi/src/test/java/cn/tedu/csmall/stock/webapi/CsmallStockWebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall.stock.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmallStockWebapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
