e3001c708149ff0dd57ee85a82bf2645d2b5bf2d
sweeterjava <sweeterjava@163.com>
2023-02-10 17:47:54 +0800

    1

---
+++ csmall-order/csmall-order-webapi/src/main/java/cn/tedu/csmall/order/webapi/CsmallOrderWebapiApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmall.order.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmallOrderWebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsmallOrderWebapiApplication.class, args);
+    }
+
+}
