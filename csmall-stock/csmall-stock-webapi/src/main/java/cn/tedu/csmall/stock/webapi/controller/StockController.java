9589f5fa4a164a663d89a0ba418227527d9c6017
sweeterjava <sweeterjava@163.com>
2023-02-11 20:16:43 +0800

    1

--- csmall-stock/csmall-stock-webapi/src/main/java/cn/tedu/csmall/stock/webapi/controller/StockController.java
+++ csmall-stock/csmall-stock-webapi/src/main/java/cn/tedu/csmall/stock/webapi/controller/StockController.java
@@ -30,20 +30,18 @@ public class StockController {
 
     @PostMapping("/reduce/count")
     @ApiOperation("减少库存数")
-// @SentinelResource注解需要标记在控制层方法上,在该方法第一次运行后,会被Sentinel仪表台检测
-// 该方法在运行前,不会出现在仪表台中
-// 括号中"减少库存数"这个描述会出现在仪表台上,代表这个方法
-// blockHandler可以设置当前控制器方法被限流时,要运行的自定义限流方法,blockError就是方法名称
-    @SentinelResource(  value = "减少库存数",
-            blockHandler = "blockError",
-            fallback = "fallbackError")
-    public JsonResult reduceCommodityCount(StockReduceCountDTO stockReduceCountDTO){
+    // @SentinelResource注解需要标记在控制层方法上,在该方法第一次运行后,会被Sentinel仪表台检测
+    // 该方法在运行前,不会出现在仪表台中
+    // 括号中"减少库存数"这个描述会出现在仪表台上,代表这个方法
+    // blockHandler可以设置当前控制器方法被限流时,要运行的自定义限流方法,blockError就是方法名称
+    @SentinelResource(value = "减少库存数", blockHandler = "blockError", fallback = "fallbackError")
+    public JsonResult reduceCommodityCount(StockReduceCountDTO stockReduceCountDTO) {
         // 调用业务逻辑层
         stockService.reduceCommodityCount(stockReduceCountDTO);
         // 随机发生异常,测试自定义降级方法效果
-        if(Math.random()<0.5){
+        if (Math.random() < 0.5) {
             throw new CoolSharkServiceException(
-                    ResponseCode.INTERNAL_SERVER_ERROR,"发生随机异常");
+                    ResponseCode.INTERNAL_SERVER_ERROR, "发生随机异常");
         }
         return JsonResult.ok("库存减少完成!");
     }
@@ -56,11 +54,11 @@ public class StockController {
     // 当上面的控制层方法发生异常时,会调用自定义降级方法
     // 实际开发中,可能调用运行一些老版本的代码,所以称之为"降级"
     public JsonResult fallbackError(StockReduceCountDTO stockReduceCountDTO,
-                                    Throwable throwable){
+                                    Throwable throwable) {
         // 输入异常信息
         throwable.printStackTrace();
         // 返回降级方法运行的信息
-        return JsonResult.failed(ResponseCode.INTERNAL_SERVER_ERROR,"服务降级");
+        return JsonResult.failed(ResponseCode.INTERNAL_SERVER_ERROR, "服务降级");
     }
 
 }
