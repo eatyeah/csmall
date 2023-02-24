5a9c4c25e7d84a8f9d86e7dadc0f51754c52b7ea
sweeterjava <sweeterjava@163.com>
2023-02-24 15:34:03 +0800

    1

--- search/src/main/java/cn/tedu/search/entity/Item.java
+++ search/src/main/java/cn/tedu/search/entity/Item.java
@@ -28,14 +28,26 @@ public class Item implements Serializable {
     // SpringDataJPA通过@Id注解标识主键
     @Id
     private Long id;
+
     // @Field是SpringData标记普通属性的注解
     // type是定义这个属性的类型,FieldType.Text是支持分词的字符串,后面要定义两个分词器
     @Field(type = FieldType.Text,
             analyzer = "ik_max_word",
             searchAnalyzer = "ik_max_word")
     private String title;
+
+    // Keyword是不需分词的字符串类型
+    @Field(type = FieldType.Keyword)
     private String category;
+
+    @Field(type = FieldType.Keyword)
     private String brand;
+
+    // Double是支持小数的数字类型
+    @Field(type = FieldType.Double)
     private Double price;
+
+    // index = false表示不需要索引,不需要索引的属性不会被搜索
+    @Field(type = FieldType.Keyword, index = false)
     private String imgUrl;
 }
