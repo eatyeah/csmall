15f1fa8a0ad273ecc8671e9ce298b40b287d7bb0
sweeterjava <sweeterjava@163.com>
2023-03-02 14:21:04 +0800

    提交代码

--- csmall-order/csmall-order-webapi/src/main/java/cn/tedu/csmall/order/webapi/controller/OrderController.java
+++ csmall-order/csmall-order-webapi/src/main/java/cn/tedu/csmall/order/webapi/controller/OrderController.java
@@ -1,10 +1,16 @@
 package cn.tedu.csmall.order.webapi.controller;
 
+import cn.tedu.csmall.commons.pojo.order.model.Order;
 import cn.tedu.csmall.commons.restful.JsonResult;
 import cn.tedu.csmall.order.service.IOrderService;
+import cn.tedu.csmall.order.webapi.service.impl.OrderServiceImpl;
+import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +21,7 @@ import cn.tedu.csmall.commons.pojo.order.dto.OrderAddDTO;
 @Api(tags = "订单管理模块")
 public class OrderController {
     @Autowired
-    private IOrderService orderService;
+    private OrderServiceImpl orderService;
 
     @PostMapping("/add")
     @ApiOperation("新增订单功能")
@@ -24,5 +30,16 @@ public class OrderController {
         return JsonResult.ok("新增订单完成!");
     }
 
+    @GetMapping
+    @ApiOperation("分页查询所有订单")
+    @ApiImplicitParams({
+            @ApiImplicitParam(value = "页码",name="page",example ="1" ),
+            @ApiImplicitParam(value = "每页条数",name="pageSize",example ="10" )
+    })
+    public JsonResult<PageInfo<Order>> pageOrder(Integer page,Integer pageSize){
+        PageInfo<Order> pageInfo=
+                orderService.getAllOrdersByPage(page, pageSize);
+        return JsonResult.ok("查询完成",pageInfo);
+    }
 
 }
