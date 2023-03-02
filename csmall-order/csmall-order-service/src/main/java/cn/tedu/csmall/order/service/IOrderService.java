93f514edf2e9f8ef609d7630531b725d43ab13b3
sweeterjava <sweeterjava@163.com>
2023-03-02 15:09:19 +0800

    提交代码

--- csmall-order/csmall-order-service/src/main/java/cn/tedu/csmall/order/service/IOrderService.java
+++ csmall-order/csmall-order-service/src/main/java/cn/tedu/csmall/order/service/IOrderService.java
@@ -2,14 +2,15 @@ package cn.tedu.csmall.order.service;
 
 
 import cn.tedu.csmall.commons.pojo.order.dto.OrderAddDTO;
+import cn.tedu.csmall.commons.pojo.order.model.Order;
+import cn.tedu.csmall.commons.restful.JsonPage;
 
 public interface IOrderService {
 
     // 声明新增订单的业务逻辑层方法
     void orderAdd(OrderAddDTO orderAddDTO);
 
-
-
-
+    // 返回JsonPage类型的分页查询订单的方法
+    JsonPage<Order> getAllOrdersByPage(Integer page, Integer pageSize);
 
 }
